a3758d717e67e1c69404e4040a1dea3ee25d6094
Michele Sessa <msessa@c2r8s6.42lisboa.com>
2021-08-18 19:22:34 +0100

    starting to create types for the minishell

--- minishell/headers/types.h
+++ minishell/headers/types.h
@@ -1,5 +1,32 @@
 #ifndef TYPES_H
 # define TYPES_H
 
+typedef enum e_token_type
+{
+	word,
+	control_operator,		/* newline, ‘||’, ‘&&’, ‘&’, ‘;’, ‘;;’, ‘;&’, ‘;;&’, ‘|’, ‘|&’, ‘(’, or ‘)’ */
+	redirection_operator,	/* '<', '>', '<<', '>>',  */
+	metacharacter			/* space, tab, newline, ‘|’, ‘&’, ‘;’, ‘(’, ‘)’, ‘<’, or ‘>’ */
+}				t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			str;
+}				t_token;
+
+typedef struct s_cmd
+{
+	char	*name;
+	char	**args;
+	char	*in;
+	char	*out;
+}				t_cmd;
+
+typedef struct s_minishell
+{
+	t_list	**cmds;
+	t_list	**tokens;
+}				t_minishell;
 
 #endif
