739bda990e13be8959b93a017587849f5c05f5bc
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-08-31 10:25:58 +0100

    further advances in parser 🧺

--- minishell/srcs/parser/parser.c
+++ minishell/srcs/parser/parser.c
@@ -11,3 +11,36 @@ t_minishell	*parser(char *line, t_minishell *minishell)
 	DEBUG(print_tokens(minishell->tokens);)
 	return (minishell);
 }
+
+/* returns a t_list **of commands.
+ * calls gets_commands once, and passes cmd_list by ref */
+static t_list **interprets_tokens(t_list **tokens)
+{
+	t_list **cmd_list;
+	cmd_list = calloc_or_exit(1, sizeof(t_list **));
+	gets_commands(tokens, cmd_list);
+}
+
+/* returns a t_list of commands.
+ * always assumes every function call is the first token */
+static void gets_command(t_list **tokens, t_list **cmd_list)
+{
+	t_cmd *cmd;
+	int i;
+
+	*command = calloc_or_exit(1, sizeof(t_cmd))
+	i = -1;
+	while (tokens && tokens[++i])
+	{
+		if (i == 0 && tokens[i]->type == WORD)
+			command->name = tokens[i]->str;
+		else
+		{
+			if (tokens[i]->type == OPERATOR) // and is valid operator
+			{
+				gets_command(&tokens[i], cmd_list);
+				while (tokens[i] !=  // closing operator
+					// advance i;
+	}
+}
+
