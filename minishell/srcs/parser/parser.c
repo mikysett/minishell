3c93aed9de00be1ad9de5d7f21de2bd4ea8ca463
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-09-02 19:04:30 +0100

    parsing of commands done? needs init of ms->inst ✋
    needs initialization of minishell struct to add back

--- minishell/srcs/parser/parser.c
+++ minishell/srcs/parser/parser.c
@@ -1,11 +1,16 @@
 #include "minishell.h"
 
+static t_list	*interprets_tokens(t_list **tokens, int cmd_id, int cmd_group, int closed);
+static t_list	*handle_command(t_list **tokens, int cmd_id, int cmd_group);
+static t_list	*handle_redir(t_list **tokens, int cmd_id, int closed);
+static void		insert_token_in_list(void *instruction, int instr_type);
+
 t_minishell	*parser(char *line, t_minishell *minishell)
 {
 	minishell->tokens = calloc_or_exit(1, sizeof(t_list *));
 	minishell->tokens = lexer(line, minishell->tokens);
 	if (prog_state(TAKE_STATE) == PROG_OK)
-		interprets_tokens(tokens, 0, 0, 0);
+		interprets_tokens(minishell->tokens, 0, 0, 0);
 	DEBUG(print_tokens(minishell->tokens);)
 	return (minishell);
 }
@@ -14,24 +19,25 @@ t_minishell	*parser(char *line, t_minishell *minishell)
  * is the list of  exaustive?
  *
  * */
-static void interprets_tokens(t_list **tokens, int cmd_id, int cmd_group, int closed)
+static t_list *interprets_tokens(t_list **tokens, int cmd_id, int cmd_group, int closed)
 {
-	t_instruction *inst;
+	t_list *curr_token;
 
-	curr_token = handle_redir(curr_token, cmd_id, cmd_group, true);
-	if (curr_token->type == WORD)
-		curr_token = handle_command(curr_token, cmd_id, cmd_group);
-	else if (is_logic_op(curr_token))
+	curr_token = *tokens;
+	//curr_token = handle_redir(curr_token, cmd_id, cmd_group, true);
+	if (((t_token *)curr_token->content)->type == WORD)
+		curr_token = handle_command(tokens, cmd_id, cmd_group);
+	else if (is_logic_op(((t_token *)curr_token->content)->str))
 		;// passing
-	else if (ft_strncmp(curr_token->str, "(", 2) == 0);
-		curr_token = gets_commands(curr_token->next, cmd_id + 1, cmd_group + 1, true);
+	else if (ft_strncmp(((t_token *)curr_token->content)->str, "(", 2) == 0)
+		curr_token = interprets_tokens(&(*tokens)->next, cmd_id + 1, cmd_group + 1, true);
 	/* this has the be thought of;
 	 * how to handle nesting?
 	 * how to avoid empty parens? */
-	else if (ft_strncmp(curr_token->str, ")", 2) == 0);
+	else if (ft_strncmp(((t_token *)curr_token->content)->str, ")", 2) == 0)
 		;// passing
 		//curr_token = gets_commands(curr_token->next, cmd_id + 1, cmd_group + 1, true);
-	curr_token = handle_redir(curr_token, cmd_id);
+	//curr_token = handle_redir(curr_token, cmd_id);
 }
 
 
@@ -39,33 +45,32 @@ static void interprets_tokens(t_list **tokens, int cmd_id, int cmd_group, int cl
  * takes the name of the command, and then the
  * length of the node sequence w/ words;
  * if length > 1, args are allocated in char** */
-static t_list	*handle_command(t_token *curr_token, int cmd_id, int cmd_group)
+static t_list	*handle_command(t_list **tokens, int cmd_id, int cmd_group)
 {
 	t_cmd	*cmd;
-	t_token *name;
 	int		length;
 	int		i;
 
-	*cmd = calloc_or_exit(1, sizeof(t_cmd));
-	length = take_length_of_command(curr_token);
-	cmd->name = (t_token *)curr_token->content->str;
+	cmd = (t_cmd *)calloc_or_exit(1, sizeof(t_cmd));
+	length = take_length_of_command(*tokens);
 	cmd->id = cmd_id;
 	cmd->group = cmd_group;
-	curr_token = curr_token->next;
+	cmd->name = ((t_token *)(*tokens)->content)->str;
+	*tokens = (*tokens)->next;
 	if (length > 1)
 		cmd->args = calloc_or_exit(length, sizeof(char **));
 	i = -1;
-	while (++i != length)
+	while (++i != length - 1)
 	{
-		cmd->args[i] = (t_token *)curr_token->content->str;
-		curr_token = curr_token->next;
+		cmd->args[i] = ((t_token *)(*tokens)->content)->str;
+		*tokens = (*tokens)->next;
 	}
 	insert_token_in_list(cmd, INSTR_CMD);
-	return (curr_token);
+	return (*tokens);
 }
 
 /* TODO might be shaky regarding "(null) < command" situations
- * will not always add a redirection; */
+ * will not always add a redirection;
 static t_list	 *handle_redir(t_list *curr_token, int cmd_id, int closed)
 {
 	t_token *token;
@@ -80,7 +85,7 @@ static t_list	 *handle_redir(t_list *curr_token, int cmd_id, int closed)
 				prog_state(PARSER_ERROR);
 				break;
 			}
-			insert_redir(token, cmd_id);
+			handle_redir(token, cmd_id);
 			curr_token = curr_token->next;
 		}
 		else
@@ -88,20 +93,20 @@ static t_list	 *handle_redir(t_list *curr_token, int cmd_id, int closed)
 		curr_token = curr_token->next;
 	}
 	return (curr_token);
-}
+}*/
 
 /* TODO this function can be generalized to accept all tokens,
  * typecasting (if needed) according to instr_type */
-static void		insert_token_in_list(t_token *token, int instr_type)
+static void		insert_token_in_list(void *instruction, int instr_type)
 {
-	t_minishell ms;
-	t_list	new_node;
+	t_minishell *ms;
+	t_list	*new_node;
 
 	ms = get_minishell(NULL);
 	if (instr_type == INSTR_CMD)
 	{
-		new_node = ft_lstnew(token);
-		if !(new_node)
+		new_node = ft_lstnew(instruction);
+		if (!(new_node))
 			return;
 		ft_lstadd_back(ms->instructions, new_node);
 	}
--- minishell/srcs/parser/parser_util.c
+++ minishell/srcs/parser/parser_util.c
@@ -4,15 +4,35 @@ int		is_redir_op(char *str)
 {
 	return (ft_strncmp(str, "<", 1) == 0 || ft_strncmp(str, ">", 1) == 0
 			|| ft_strncmp(str, "<<", 2) == 0 || ft_strncmp(str, ">>", 2) == 0
-			|| ft_strncmp(str, "|", 1) == 0)
+			|| ft_strncmp(str, "|", 1) == 0);
 }
 
 int		is_logic_op(char *str)
 {
-	return (ft_strncmp(str, "||", 2) == 0 || ft_strncmp(str, "&&", 2) == 0)
+	return (ft_strncmp(str, "||", 2) == 0 || ft_strncmp(str, "&&", 2) == 0);
 }
 
 int		is_paren_op(char *str)
 {
-	return (ft_strncmp(str, "(", 1) == 0 || ft_strncmp(str, ")", 1) == 0)
+	return (ft_strncmp(str, "(", 1) == 0 || ft_strncmp(str, ")", 1) == 0);
+}
+
+/* gets a node object, and retrieves the amount of words
+ * in that command */
+int		take_length_of_command(t_list *node)
+{
+	int i;
+	t_token *token;
+
+	i = 0;
+	if (!node)
+		return (0);
+	while (node)
+	{
+		token = (t_token *)node->content;
+		if (token->type == WORD)
+			i++;
+		node = node->next;
+	}
+	return (i);
 }
