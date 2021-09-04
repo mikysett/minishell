48a9dcc3c2e2587e9b8264547713aa758f244ded
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-09-04 18:23:55 +0100

    finishes parsing of redir 🎉

--- minishell/srcs/parser/parser.c
+++ minishell/srcs/parser/parser.c
@@ -5,6 +5,7 @@ static t_list			*handle_command(t_list **tokens, int cmd_id, int cmd_group);
 static t_list			*look_for_redir(t_list **tokens, int cmd_id, int closed);
 static void				insert_token_in_list(void *instruction, int instr_type);
 static t_redirect		*handle_redir(t_list *curr_token, int cmd_id, int before);
+static t_cmd			*init_instruction(t_minishell *ms);
 
 t_minishell	*parser(char *line, t_minishell *minishell)
 {
@@ -18,26 +19,18 @@ t_minishell	*parser(char *line, t_minishell *minishell)
 }
 
 /* always assumes every function call is the first token
- * is the list of  exaustive?
- *
- * */
+ * TODO ensure comparing of ( ) with their types!
+ * Legal case: < test-1 | wc !! */
 static t_list *interprets_tokens(t_list *curr, int cmd_id, int cmd_group, int closed)
 {
-	t_token *curr_token;
+	t_token			*curr_token;
 	const t_list	*start_pos = curr;
-	curr_token = (t_token *)curr->content;
 
+	curr_token = (t_token *)curr->content;
 	curr = look_for_redir(curr_token, cmd_id);
 	curr_token = (t_token *)curr->content;
 	if (prog_state(TAKE_STATE) != PROG_OK)
 		return (NULL);
-	// Legal case: < test-1 | wc !!
-	if (curr != start_pos && curr_token->type == OPERATOR)
-	{
-		prog_state(PARSER_ERROR);
-		return (NULL);
-	}
-
 	if (curr_token->type == WORD)
 		curr_token = handle_command(curr, cmd_id, cmd_group);
 	else if (is_logic_op(curr_token->str))
@@ -57,13 +50,11 @@ static t_list *interprets_tokens(t_list *curr, int cmd_id, int cmd_group, int cl
 	// check if pipe
 }
 
+/* initiates an instruction outside of the main
+ * handling functions; this ensures allocation is retrievable
+ * in case of failure at the handler function */
 
-/* TODO how to handle malloc in case of failure?
- * takes the name of the command, and then the
- * length of the node sequence w/ words;
- * if length > 1, args are allocated in char** */
-
-t_cmd	*init_instruction(t_minishell *ms)
+static t_cmd	*init_instruction(t_minishell *ms)
 {
 	t_instruction	*instr;
 	t_list			*new_instr;
@@ -74,6 +65,7 @@ t_cmd	*init_instruction(t_minishell *ms)
 		ft_error_exit(MEMORY_FAIL);
 	ft_lstadd_back(ms->instructions, new_instr);
 	instr->cmd = calloc_or_exit(1, sizeof(t_cmd));
+	return (instr->cmd);
 }
 
 static t_list	*handle_command(t_list **tokens, int cmd_id, int cmd_group)
@@ -82,9 +74,7 @@ static t_list	*handle_command(t_list **tokens, int cmd_id, int cmd_group)
 	int		length;
 	int		i;
 
-	init_instruction(get_minishell(NULL));
-
-	cmd = calloc_or_exit(1, sizeof(t_cmd));
+	cmd = init_instruction(get_minishell(NULL));
 	length = take_length_of_command(*tokens);
 	cmd->id = cmd_id;
 	cmd->group = cmd_group;
@@ -98,13 +88,12 @@ static t_list	*handle_command(t_list **tokens, int cmd_id, int cmd_group)
 		cmd->args[i] = ((t_token *)(*tokens)->content)->str;
 		*tokens = (*tokens)->next;
 	}
-	insert_token_in_list(cmd, INSTR_CMD);
 	return (*tokens);
 }
 
 /* TODO might be shaky regarding "(null) < command" situations
  * the function checks for consecutive redirections, whenever appliable? */
-static t_list	 *look_for_redir(t_list *curr_token, int cmd_id, int before)
+static t_list	*look_for_redir(t_list *curr_token, int cmd_id)
 {
 	t_token		*token;
 	t_redirect	*redir;
@@ -114,36 +103,42 @@ static t_list	 *look_for_redir(t_list *curr_token, int cmd_id, int before)
 		token = (t_token *)curr_token->content;
 		if (is_redir_op(token->str))
 		{
-			if (!token->next || ((t_token *)token->next)->type != WORD)
-			{
-				prog_state(PARSER_ERROR);
-				break;
-			}
-			handle_redir(curr_token, cmd_id, before);
+			curr_token = handle_redir(curr_token, cmd_id, before);
 			curr_token = curr_token->next;
 		}
 		else
 			break;
-		curr_token = curr_token->next;
 	}
 	return (curr_token);
 }
 
-static t_redirect	 *handle_redir(t_list *curr_token, int cmd_id, int before)
+/* returns the token with the text, after the redir token
+ * merge this with the previous function, for increased sexyness of the code */
+static t_list	*handle_redir(t_token *operator, t_token *file_name, int cmd_id)
 {
-	t_redirect *redir;
+	t_redirect	*redir;
+	t_token		*next;
 
-	if (ft_strncmp(token->str == "|", 1) == 0)
+	redir = init_instruction(get_minishell(NULL));
+	if (token->type == OPERATOR && ft_strncmp(token->str, "<", 2) == 0)
+		redir->type = REDIR_IN;
+	else if (token->type == OPERATOR && ft_strncmp(token->str, ">", 2) == 0)
+		redir->type = REDIR_OUT;
+	else if (token->type == OPERATOR && ft_strncmp(token->str, "<<" , 3) == 0)
+		redir->type = REDIR_HERE_DOC;
+	else if (token->type == OPERATOR && ft_strncmp(token->str, ">>" , 3) == 0)
+		redir->type = REDIR_OUT_APPEND;
+	/* no redir found! */
+	if (curr_token->next != NULL)
+		next = ((t_token *)curr_token->next)->content;
+	else if ((t_list *)curr_token->next == NULL || next->type != WORD)
 	{
-		/* there was nothing preceding pipe! */
-		if (before)
-		{
-			prog_state(PARSER_ERROR);
-			return (NULL);
-		}
-		*redir = calloc_or_exit(1, sizeof(t_redirect));
-		/* every pipe originates two redir instructions, in and out */
+		prog_state(PARSER_ERROR);
+		return;
 	}
+	redir->file_name = ft_strdup(next->str);
+	redir->cmd_id = cmd_id;
+	return (curr_token->next);
 }
 
 /* TODO this function can be generalized to accept all tokens,
