3a583c9c8aeba0b027fffac68c6fa08eb4822016
mikysett <mikysett@gmail.com>
2021-09-09 14:48:55 +0100

    echo builtin fixed (now respecting standard behavior)

--- minishell/srcs/cmd_builtin/echo_builtin.c
+++ minishell/srcs/cmd_builtin/echo_builtin.c
@@ -1,20 +1,35 @@
 #include "minishell.h"
 
+static bool	get_print_newline(int argc, char **argv);
+
 int	echo_builtin(char **argv)
 {
 	const int	args_nb = args_count(argv);
-	bool		print_newline;
+	const bool	print_newline = get_print_newline(args_nb, argv);
+	bool		something_printed;
+	int			i;
 
-	print_newline = true;
-	if (args_nb > 2)
-		if (!ft_strncmp("-n", argv[1], 3))
-		{
-			argv++;
-			print_newline = false;
-		}
-	while (*(++argv))
-		write(STDIN_FILENO, *argv, ft_strlen(*argv));
+	if (print_newline == false)
+		i = 1;
+	else
+		i = 0;
+	something_printed = false;
+	while (++i < args_nb)
+	{
+		if (something_printed)
+			write(STDIN_FILENO, " ", 1);
+		else
+			something_printed = true;
+		write(STDIN_FILENO, argv[i], ft_strlen(argv[i]));
+	}
 	if (print_newline)
 		write(STDIN_FILENO, "\n", 1);
 	return (EXIT_SUCCESS);
+}
+
+static bool	get_print_newline(int argc, char **argv)
+{
+	if (argc >= 2 && !ft_strncmp("-n", argv[1], 3))
+			return (false);
+	return (true);
 }
