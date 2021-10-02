c9c6f4cf3000bd934fd0139cdb22de09f0858158
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-10-02 11:50:32 +0100

    not much progress; draft on grammar validation 📝

--- minishell/srcs/parser/validate_grammar.c
+++ minishell/srcs/parser/validate_grammar.c
@@ -5,10 +5,7 @@ typedef struct s_grammar_vars {
 	t_list  *curr;
 	t_list	*prev;
 	t_list	*next;
-	int		word;
-	int		pipe;
-	int		redir;
-	int		close_paren;
+	int		paren_count;
 }				t_grammar_vars;
 
 void	advance_node(t_grammar_vars *grammar)
@@ -37,8 +34,10 @@ void	initializes_grammar_vars(t_grammar_vars *grammar, t_list *curr_node)
 
 /* > |
  * redirs require word after
- *
+ * there cannot be consecutive operators, unless it is a redir
  */
+
+
 
 void	validate_grammar(t_list *curr_node)
 {
@@ -48,15 +47,36 @@ void	validate_grammar(t_list *curr_node)
 	while (curr_node && prog_state(TAKE_STATE) == PROG_OK)
 	{
 		/* no rules for words. traverse them normally. */
-		if (get_token(curr_node)->type == WORD);
 		if (get_token(curr_node)->type == OPERATOR)
 		{
+			// ✅ redirections NEED
 			if (is_redir_op(get_token(curr_node)))
 				/* unexpected token is current_token */
 				if (!grammar->next || (get_token(grammar->next)->type != WORD))
 					prog_state(PARSER_ERROR);
 
-		advance_node(grammar);
-
+			else if (is_logical_op(get_token(grammar_curr)))
+			{
+				if (!grammar->prev)
+					prog_state(PARSER_ERROR);
+				/* unexpected token is current_token */
+				if (is_logical_op(get_token(grammar->next)))
+					prog_state(PARSER_ERROR);
+			}
+			else if (ft_strncmp(get_token(grammar->curr)->str, "|"))
+			{
+				if (!grammar->prev)
+					prog_state(PARSER_ERROR);
+				else if (grammar->next && (ft_strncmp(get_token(grammar->next)->str, "|"))
+					prog_state(PARSER_ERROR);
+			}
+			else if (ft_strncmp(get_token(grammar->curr)->str, "("))
+			{
+				if (!grammar->next)
+					prog_state(PARSER_ERROR);
+				if (!grammar->prev)
+					prog_state(PARSER_ERROR);
+			}
+			advance_node(grammar);
 	}
 }
