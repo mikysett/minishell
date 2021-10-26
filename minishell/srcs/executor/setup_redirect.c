984436a837ed971d2c156fd5ae9f5f16ad0ebe96
Michele Sessa <mikysett@gmail.com>
2021-10-26 11:32:51 +0100

    Fixed bug when heredoc is used in conjunction with a pipe

--- minishell/srcs/executor/setup_redirect.c
+++ minishell/srcs/executor/setup_redirect.c
@@ -1,9 +1,11 @@
 #include "minishell.h"
 
+static bool	setup_heredoc_redir(t_std_io *std_io, t_list *curr, int id,
+				t_has_redirs *has_redirs);
 static bool	setup_pipe_redir(t_std_io *std_io, t_list *curr, int id,
-	t_has_redirs *has_redirs);
+				t_has_redirs *has_redirs);
 static bool	setup_other_redir(t_std_io *std_io, t_list *curr, int id,
-	t_has_redirs *has_redirs);
+				t_has_redirs *has_redirs);
 
 bool	setup_redirect(t_std_io *std_io, t_list **redirect, int id)
 {
@@ -11,6 +13,8 @@ bool	setup_redirect(t_std_io *std_io, t_list **redirect, int id)
 
 	has_redirs.in = false;
 	has_redirs.out = false;
+	if (!setup_heredoc_redir(std_io, *redirect, id, &has_redirs))
+		return (false);
 	if (!setup_pipe_redir(std_io, *redirect, id, &has_redirs))
 		return (false);
 	if (!setup_other_redir(std_io, *redirect, id, &has_redirs))
@@ -19,11 +23,34 @@ bool	setup_redirect(t_std_io *std_io, t_list **redirect, int id)
 	return (true);
 }
 
+static bool	setup_heredoc_redir(t_std_io *std_io, t_list *curr, int id,
+	t_has_redirs *has_redirs)
+{
+	t_redirect		*curr_redir;
+
+	while (curr)
+	{
+		curr_redir = (t_redirect *)curr->content;
+		if (curr_redir->cmd_id == id
+			&& curr_redir->type == REDIR_HERE_DOC)
+		{
+			has_redirs->in = true;
+			if (!make_redir(std_io, curr_redir))
+				return (false);
+		}
+		else if (curr_redir->cmd_id > id)
+			break ;
+		curr = curr->next;
+	}
+	return (true);
+}
+
+
 static bool	setup_pipe_redir(t_std_io *std_io, t_list *curr, int id,
 	t_has_redirs *has_redirs)
 {
 	t_redirect		*curr_redir;
-	
+
 	while (curr)
 	{
 		curr_redir = (t_redirect *)curr->content;
@@ -49,15 +76,16 @@ static bool	setup_other_redir(t_std_io *std_io, t_list *curr, int id,
 	t_has_redirs *has_redirs)
 {
 	t_redirect		*curr_redir;
-	
+
 	while (curr)
 	{
 		curr_redir = (t_redirect *)curr->content;
 		if (curr_redir->cmd_id == id
-			&& curr_redir->type != REDIR_PIPE_IN
-			&& curr_redir->type != REDIR_PIPE_OUT)
+			&& (curr_redir->type == REDIR_IN
+				|| curr_redir->type == REDIR_OUT
+				|| curr_redir->type == REDIR_OUT_APPEND))
 		{
-			if (is_redir_in(curr_redir->type))
+			if (curr_redir->type == REDIR_IN)
 				has_redirs->in = true;
 			else
 				has_redirs->out = true;
